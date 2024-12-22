9ae04bb8efe314b85a25a095a940b2cade7eafe6
mrrobot69-pixel <75664037+mrrobot69-pixel@users.noreply.github.com>
2024-12-22 04:15:49 +0000

    variables created

---
+++ 01basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 6953
+let accountEmail = "abc@gmail.com"
+var pass = "12345"
+accountCity = "Jaipur"
+
+//accountId = 133 //not allowed
+accountEmail = "hemant@gmail.com"
+pass = "hemant123"
+accountCity="Navi Mumbai"
+
+console.log(accountEmail);
+
+console.table([accountId,accountEmail,pass,accountCity])
+
+
