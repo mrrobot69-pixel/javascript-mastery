aebf4460dce7e2d82d336602bc6cf404fe8d63fa
mrrobot69-pixel <75664037+mrrobot69-pixel@users.noreply.github.com>
2024-12-23 01:24:20 +0000

    datatypes

--- 01basics/01_variables.js
+++ 01basics/01_variables.js
@@ -4,11 +4,16 @@ var pass = "12345"
 accountCity = "Jaipur"
 
 //accountId = 133 //not allowed
+
 accountEmail = "hemant@gmail.com"
 pass = "hemant123"
 accountCity="Navi Mumbai"
 
 console.log(accountEmail);
+
+/*prefer not to use var
+because of issue in block scope and functional scope
+*/
 
 console.table([accountId,accountEmail,pass,accountCity])
 
---
+++ 01basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all js code as newer version
+
+//alert(3 + 3)  // we are using nodejs, not browser
+
+console.log(3 
+    + 3) // code readability should be high
+    
+console.log("Hemant")
+
+let name = "Hemant"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//string => ""
+//number => 2 to power 53
+//boolean => true or false
+//undefined => 
+//null => standalone value
+//symbol => unique
+
+//object
+
+console.log(typeof undefined) // undefined
+consoele.log(typeof null) //object
+
+console.log(typeof name)
+console.table([typeof name, typeof age, typeof isLoggedIn, typeof state])
+
